dce4cfd90cdc4c4d5c08cb993515ce7f5bed90de
wangjianhui2464 <wangjianhui2464@gmail.com>
2018-04-09 14:37:22 +0800

    add jest ,learn jest.

---
+++ test/sum.test.js
@@ -0,0 +1,42 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 equals 3 ', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
+
+test('object assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 0; a < 10; a++) {
+    for (let b = 0; b < 10; b++) {
+      expect(a + b).not.toBe(100);
+    }
+  }
+});
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).toBe(0);
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
